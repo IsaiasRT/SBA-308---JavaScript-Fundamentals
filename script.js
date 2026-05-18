4eb6328696909a1790f32eaaa9cf6edf5b9190d5
IsaiasRT <isaiasruizt@gmail.com>
2026-05-18 14:53:04 -0400

    function that throws an error when assignmentgroup id and CourseInfo don't match

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ const AssignmentGroup = {
   ]
 };
 
+
+
 // The provided learner submission data.
 const LearnerSubmissions = [
   {
@@ -75,6 +77,39 @@ const LearnerSubmissions = [
   }
 ];
 
+
+//Function that throws an error if an AssignmentGroup does not belong to CourseInfo.
+function validCourseId(CourseInfo, AssignmentGroup, LearnerSubmissions){
+   const validId = CourseInfo.id === AssignmentGroup.course_id;
+   if(validId === false){
+    console.log("This Id is incorrect, please try again.")
+   }
+}
+
+
+//Funtion that catch potential errors using try/catch to handle them.
+
+//Function that mannage dates of the assignments submissions.
+
+
+
+    // the ID of the learner for which this data has been collected ("id": number,)
+    // the learner’s total, weighted average, in which assignments
+    //  with more points_possible should be counted for more
+    // e.g. a learner with 50/100 on one assignment and 190/200 on another
+    // would have a weighted average score of 240/300 = 80%. ("avg": number,)
+
+
+    // each assignment should have a key with its ID,
+    // and the value associated with it should be the percentage that
+    // the learner scored on the assignment ((submission.score / points_possible) <assignment_id>: number,))
+    // if an assignment is not yet due, it should not be included in either
+    // the average or the keyed dictionary of scores
+
+
+
+
+//function to get the data of the Learners
 function getLearnerData(course, ag, submissions) {
   // here, we would process this data to achieve the desired result.
   const result = [
